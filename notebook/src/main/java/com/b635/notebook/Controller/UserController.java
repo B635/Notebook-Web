3c8599e543372ad1b7e9bb8432178b7d2dafdf39
B635 <frankxck@gmail.com>
2022-06-10 15:16:01 +0800

    Update: 完善 用户相关接口并进行测试，测试通过

--- notebook/src/main/java/com/b635/notebook/Controller/UserController.java
+++ notebook/src/main/java/com/b635/notebook/Controller/UserController.java
@@ -1,8 +1,9 @@
 package com.b635.notebook.Controller;
 
 import com.b635.notebook.Model.entity.User;
-import com.b635.notebook.Model.vo.Result;
 import com.b635.notebook.Service.impl.UserServiceimpl;
+import com.b635.notebook.utils.R;
+import com.b635.notebook.utils.RUtils;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,38 +11,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 public class UserController {
     @Autowired
     private UserServiceimpl userService;
 
     @RequestMapping("/login")
-    public Result login(@RequestBody User user) {
-        Result result = new Result();
+    public R login(@RequestBody User user) {
         User one = userService.getOne(
                 new QueryWrapper<User>()
                         .eq("username", user.getUserName())
                         .eq("password", user.getPassWord()));
         if(one!=null){
-            return result.ok(null,"登录成功");
+            return RUtils.result("登录成功");
         }else{
-            return result.fail(null,"用户名或密码错误");
+            return RUtils.result("用户名或密码错误");
         }
     }
 
     @RequestMapping("/register")
-    public Result register(@RequestBody User user) {
+    public R register(@RequestBody User user) {
         userService.save(user);
-        return Result.ok(null,"success");
+        return RUtils.result("注册成功");
     }
 
     @RequestMapping("/isExist")
-    public Result isExist(@RequestBody User user) {
+    public R isExist(@RequestBody User user) {
         User one = userService.getOne(new QueryWrapper<User>().eq("username", user.getUserName()));
         if(one!=null){
-            return Result.fail(null,"用户名已存在");
+            return RUtils.result("用户名已存在");
         }else{
-            return Result.ok(null,"用户名不存在");
+            return RUtils.result("用户名不存在");
         }
     }
 
