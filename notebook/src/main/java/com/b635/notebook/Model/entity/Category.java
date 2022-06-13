405d544aca88b329d5ce5eaae0b1da24a30c2f05
B635 <frankxck@gmail.com>
2022-06-13 11:33:25 +0800

    Update: 完善 model 中的类

--- notebook/src/main/java/com/b635/notebook/Model/entity/Category.java
+++ notebook/src/main/java/com/b635/notebook/Model/entity/Category.java
@@ -1,5 +1,7 @@
 package com.b635.notebook.Model.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,6 +11,7 @@ import lombok.Data;
 @TableName(schema = "notebook")
 public class Category {
     // 分类id
+    @TableId(value="id",type = IdType.AUTO )
     private Integer id;
 
     // 分类名
--- notebook/src/main/java/com/b635/notebook/Model/entity/Tag.java
+++ notebook/src/main/java/com/b635/notebook/Model/entity/Tag.java
@@ -1,5 +1,7 @@
 package com.b635.notebook.Model.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,6 +11,7 @@ import lombok.Data;
 @TableName(schema = "notebook")
 public class Tag {
     // 标签id
+    @TableId(value="id",type = IdType.AUTO )
     private Integer id;
 
     // 标签名
